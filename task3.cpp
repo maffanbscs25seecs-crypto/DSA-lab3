40f7615be5aa0450e372a27e61978d9f165f0224
maffanbscs25seecs-crypto <maffan.bscs25seecs@seecs.edu.pk>
2026-09-22 15:23:48 +0500

    Create task3.cpp

---
+++ task3.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Student {
+	int rollNo;
+	string name;
+	float marks;
+};
+
+int main() {
+	//create student record using new
+	Student* s = new Student{};
+	//check that s is not nullptr before accessing its fields
+	if (s != nullptr) {
+		//input roll number, full name, marks
+		cout << "Enter the student's roll number: ";
+		cin >> s->rollNo;
+		cout << "Enter the student's full name: ";
+		getline(cin >> ws, s->name);
+		cout << "Enter the student's marks: ";
+		cin >> s->marks;
+
+		//display all details using arrow operator
+		cout << endl << "Student details:" << endl;
+		cout << "Roll number: " << s->rollNo << endl;
+		cout << "Name: " << s->name << endl;
+		cout << "Marks: " << s->marks << endl;
+	}
+	else
+		cout << "Error" << endl;
+
+	//release memory and update the student pointer to nullptr
+	delete s;
+	s = nullptr;
+
+	return 0;
+}
